a60eec048054f844d944c6d605af07223f539c6a
zs <Z>
2021-12-13 20:31:15 +0200

    Fixed height scene slider

--- Assets/Scripts/ChangeHeight.cs
+++ Assets/Scripts/ChangeHeight.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeHeight : MonoBehaviour
 {
     public GameObject cameraGameObject;
+    public Slider heightSlider;
+    private Vector3 initialValue;
+    private float initialSliderValue;
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialValue = cameraGameObject.transform.position;
+        initialSliderValue = (float)heightSlider.value;
     }
 
     // Update is called once per frame
@@ -22,5 +27,11 @@ public class ChangeHeight : MonoBehaviour
         Vector3 position = cameraGameObject.transform.position;
         position.y = newValue;
         cameraGameObject.transform.position = position;
+    }
+
+    public void ResetValue(){
+
+        cameraGameObject.transform.position = initialValue;
+        heightSlider.value = initialSliderValue;
     }
 }
